5a1e1dd5623d8a8aa44aa7f5943242ae14a45dab
Bhavan Kumar V <bhavankumarcse2020@.com>
2024-04-22 11:17:01 +0530

    day 9 - create employee service created

---
+++ Day9/RefundManagementApplication/RefundManagementBLLibrary/EmailAddressAlreadyInUseException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefundManagementBLLibrary
+{
+    internal class EmailAddressAlreadyInUseException : Exception
+    {
+        string ErrorMessage;
+        public EmailAddressAlreadyInUseException()
+        {
+            ErrorMessage = "The email address you've provided is already used by an another employee. Try something new!";
+        }
+        public override string Message => ErrorMessage;
+    }
+}
---
+++ Day9/RefundManagementApplication/RefundManagementBLLibrary/EmployeeBL.cs
@@ -0,0 +1,37 @@
+﻿using RefundManagementDALLibrary;
+using RefundManagementModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefundManagementBLLibrary
+{
+    internal class EmployeeBL : IEmployeeService
+    {
+        readonly IRepository<int, Employee> _employeeRepository;
+
+        public EmployeeBL()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public Employee CreateEmployeeAccount(Employee newEmployee)
+        {
+            List<Employee>? allEmployees = _employeeRepository.GetAll();
+
+            if (allEmployees != null)
+            {
+                foreach (var employee in allEmployees)
+                {
+                    if (employee.Email == newEmployee.Email) throw new EmailAddressAlreadyInUseException();
+                }
+            }
+
+            allEmployees.Add(newEmployee);
+
+            return newEmployee;
+        }
+    }
+}
