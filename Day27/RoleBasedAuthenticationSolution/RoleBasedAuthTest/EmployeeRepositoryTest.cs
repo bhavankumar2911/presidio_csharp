4750412914ad5e15740765b3563f84d25d5e3c15
Bhavan Kumar V <bhavankumarcse2020@.com>
2024-05-22 14:25:14 +0530

    mini project erd added

---
+++ Day27/RoleBasedAuthenticationSolution/RoleBasedAuthTest/EmployeeRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleBasedAuthenticationAPI.Models;
+using RoleBasedAuthenticationAPI.Repository.Interfaces;
+using RoleBasedAuthenticationAPI.Repository;
+using RoleBasedAuthenticationAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoleBasedAuthenticationAPI.Context;
+using RoleBasedAuthenticationAPI.Services;
+
+namespace RoleBasedAuthTest
+{
+    internal class EmployeeRepositoryTest
+    {
+        RoleBasedAuthContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder().UseInMemoryDatabase("dummyDB");
+            context = new RoleBasedAuthContext(optionsBuilder.Options);
+        }
+
+        [Test]
+        public async Task GetEmployeeTest()
+        {
+            //Arrange
+            IRepository<int, Employee> employeeRepo = new EmployeeRepository(context);
+
+            Employee employee = await employeeRepo.Add(new Employee
+            {
+                Name = "test employee",
+                Age = 21,
+                Role = "user",
+                Email = "test@gmail.com",
+                IsActive = true,
+            });
+
+            //Assert
+            Assert.IsNotNull(employee);
+
+        }
+    }
+}
